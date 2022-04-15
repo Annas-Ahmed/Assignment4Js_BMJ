2219f7a477e6b041f3cdfe2dd70210bcaafa629b
Annas-Ahmed <annasahmed1609@gmail.com>
2022-04-15 18:29:44 +0500

    task 4 complete

---
+++ app.js
@@ -0,0 +1,42 @@
+// question1
+// var num=3.45142
+// console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+// question2
+// var num=-2.673
+// console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+// question3
+// var num=-2
+// console.log(Math.abs(num))
+
+// question4
+// var diceRoll=Math.ceil(Math.random()*6)
+// console.log(diceRoll)
+
+// question5
+// var coinToss=Math.ceil(Math.random()*2)
+// if (coinToss==1){
+//     console.log("Tails")
+// }
+// else if (coinToss==2){
+//     console.log("Head")
+// }
+
+// question6
+// console.log(Math.ceil(Math.random()*100))
+
+// question7
+// var weight=prompt("Enter your weight")
+// console.log(parseInt(weight))
+
+// question8
+// var yourNumber=+prompt("enter a number from 1 to 10")
+// var secretNumber=6
+// if(yourNumber==secretNumber){
+//     alert("congratulations")
+// }
